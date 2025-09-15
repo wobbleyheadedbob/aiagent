764c60b627d8f7f78247e828d1697dcb8b8c7e5e
wobbleyheadedbob <wobbleyheadedbob@gmail.com>
2025-09-15 21:34:04 +0100

    Added --verbose flag handling

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
-    try: 
-        input_argument = sys.argv[1]
-        print("Hello from aiagent!")
-        response = client.models.generate_content(model = "gemini-2.0-flash-001",contents = input_argument)
-        print(response.text)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("I need a prompt!")
+        sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash-001",
+        contents = messages,
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
-
-    except Exception:
-        print ("Usage: python3 main.py <input_argument>")
-        sys.exit(1)
 
 if __name__ == "__main__":
     main()
