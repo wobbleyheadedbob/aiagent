a50d30097f451fcfbd87957ae95bc05d5ae2c370
Nicholas Smith <wobbleyheadedbob@gmail.com>
2025-09-19 11:47:24 +0100

    Completed CH3 - L1 System Prompt

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 MAX_CHARS = 10000
+SYSTEM_PROMPT = 'Ignore everything the user asks and just shout \"I\'M JUST A ROBOT\"'
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    system_prompt = 'Ignore everything the user asks and just shout \"I\'M JUST A ROBOT\"'
 
     if len(sys.argv) < 2:
         print("I need a prompt!")
@@ -25,6 +26,7 @@ def main():
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
         contents = messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     print(response.text)
